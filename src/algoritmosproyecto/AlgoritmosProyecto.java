2c4c5308c942ac32b1d5176e9d087fbf034c3f5c
dianacopacondori <cvdianacarolina26@gmail.com>
2025-09-15 23:26:55 -0500

    natural sort

--- src/algoritmosproyecto/AlgoritmosProyecto.java
+++ src/algoritmosproyecto/AlgoritmosProyecto.java
@@ -14,9 +14,9 @@ import java.util.Scanner;
 public class AlgoritmosProyecto {
 
     public static void main(String[] args) throws IOException {
-        int[] vector1 = {2, 5, 6, 8, 9, 4, 3, 11};
-        int[] vector2 = {2, 5, 9, 0, 1, 4, 3, 10, 12};
-        int[] vector3 = {2, 5, 6, 8, 9, 4, 3, 11};
+        int[] vector1 = {2, 5, 6, 8, 9, 4, 3, 11, 17, 15, 19};
+        int[] vector2 = {2, 5, 9, 0, 1, 4, 3, 10, 12, 8, 7};
+        int[] vector3 = {2, 5, 6, 8, 9, 4, 3, 11, 17, 15, 19};
 
         Scanner consola = new Scanner(System.in);
         System.out.println("----MENU----");
